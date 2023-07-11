9329103b858b4202ff46392c407d5ae565cefbdf
Ahmed Al Apon <86314645+xioned@users.noreply.github.com>
2023-07-11 00:25:49 -0700

    l

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float enemySpawnRate;
+
+    void Start()
+    {
+        
+    }
+    
+}
